3c310436f9cdc4e4d035dad914ee467fa9eca181
Mike Brady <mbrady@archsys.io>
2023-07-27 15:26:14 -0400

    reformat using black

--- archfx_cloud/scripts/query_factory_map.py
+++ archfx_cloud/scripts/query_factory_map.py
@@ -7,11 +7,11 @@ import pickle
 
 DEBUG = True
 
-class QueryFactoryMap:
 
+class QueryFactoryMap:
     def __init__(self, email: str):
         self._email = email
-        self.token_filename = '.cloud_api_token'
+        self.token_filename = ".cloud_api_token"
         self.__initialize_connection()
         self._sites = {}
         self._tree = {}
@@ -21,7 +21,7 @@ class QueryFactoryMap:
 
     def __initialize_connection(self):
         # api = Api('https://arch.archfx.io')
-        api = Api('https://flex.archfx.io')
+        api = Api("https://flex.archfx.io")
 
         passwd = self.get_password()
         ok = api.login(email=self._email, password=passwd)
@@ -29,30 +29,26 @@ class QueryFactoryMap:
             print("logged in successfully!")
             self.api = api
             # api.logout()
-    
 
     def read_enc_passwd(self):
-        with open('.archfx_enc_passwd', 'rb') as reader:
+        with open(".archfx_enc_passwd", "rb") as reader:
             return reader.read()
-
 
     def get_password(self) -> str:
         key = self.read_token_file()
         f = Fernet(key)
-        secret = f.encrypt(b'Gamma!23')
+        secret = f.encrypt(b"Gamma!23")
         token = self.read_enc_passwd()
         passwd = f.decrypt(token)
-        passwd = passwd.decode()    # convert to string
+        passwd = passwd.decode()  # convert to string
         assert isinstance(passwd, str)
         return passwd
-
-
 
     def read_token_file(self):
         filename = os.path.abspath(self.token_filename)
         print(f"filename: {filename}")
 
-        with open(filename, 'rb') as reader:
+        with open(filename, "rb") as reader:
             key = reader.read()
             return key
 
@@ -60,24 +56,24 @@ class QueryFactoryMap:
         result_dict = self.api.org.get()
         assert isinstance(result_dict, dict)
         # print(result_dict)
-        if 'results' not in result_dict:
+        if "results" not in result_dict:
             return None
-        results = result_dict['results']
+        results = result_dict["results"]
         org_name_list = []
         for n, result in enumerate(results):
-            org_name = result.get('slug')
+            org_name = result.get("slug")
             if DEBUG:
                 if org_name != "flex-smt":
                     continue
-            result.pop('avatar')
-            if 'thumbnail' in result: result.pop('thumbnail')
+            result.pop("avatar")
+            if "thumbnail" in result:
+                result.pop("thumbnail")
             print("")
             print(n, result)
             org_name_list.append(org_name)
         return org_name_list
 
-
-    def query_all_sites(self, org_name_list = List[str]):
+    def query_all_sites(self, org_name_list=List[str]):
         for org_name in org_name_list:
             self._tree[org_name] = {}
             print("#" * 140)
@@ -86,37 +82,40 @@ class QueryFactoryMap:
     def query_specific_site(self, org_name: str):
         result_dict: Dict = self.api.site.get(org=org_name)
         assert isinstance(result_dict, dict)
-        if 'results' not in result_dict:
+        if "results" not in result_dict:
             return None
-        results = result_dict['results']
+        results = result_dict["results"]
         for n, result in enumerate(results):
-            site_slug_id = result['id']
+            site_slug_id = result["id"]
             if DEBUG:
-                if site_slug_id != 'ps--0000-0027':
+                if site_slug_id != "ps--0000-0027":
                     continue
-            org = str(result.get('org', ''))
-            site_name = result.get('name', '')
+            org = str(result.get("org", ""))
+            site_name = result.get("name", "")
 
-            basic_site_info = {'site_name': site_name, 'site_slug_id': site_slug_id, 'org': org}
+            basic_site_info = {"site_name": site_name, "site_slug_id": site_slug_id, "org": org}
             self._sites[site_slug_id] = basic_site_info
             if not site_slug_id in self._tree[org]:
                 self._tree[org][site_slug_id] = {}
 
-
     def query_all_areas(self):
         for site_slug_id in self._sites.keys():
             result_dict: Dict = self.api.area.get(site=site_slug_id)
-            for n, result in enumerate(result_dict['results']):
-                area_name = result.get('name', '')
-                area_slug_id = result.get('slug')
-                site_slug_id = result.get('site')
-                area_type = result.get('area_type')
+            for n, result in enumerate(result_dict["results"]):
+                area_name = result.get("name", "")
+                area_slug_id = result.get("slug")
+                site_slug_id = result.get("site")
+                area_type = result.get("area_type")
                 site_info: Dict = self._sites[site_slug_id]
-                basic_area_info = {'area_name': area_name, 'area_slug_id': area_slug_id, 'area_type': area_type,
-                                   'site_slug_id': site_slug_id}
+                basic_area_info = {
+                    "area_name": area_name,
+                    "area_slug_id": area_slug_id,
+                    "area_type": area_type,
+                    "site_slug_id": site_slug_id,
+                }
                 basic_area_info.update(site_info)
                 self._area[area_slug_id] = basic_area_info
-                org = site_info['org']
+                org = site_info["org"]
                 if area_slug_id not in self._tree[org][site_slug_id]:
                     self._tree[org][site_slug_id][area_slug_id] = {}
 
@@ -124,32 +123,34 @@ class QueryFactoryMap:
                 # print("")
                 # print(f"n: {n}, ", basic_area_info)
 
-
     def query_all_lines(self):
         for area_slug_id in self._area.keys():
             print("#" * 140)
             print("area_slug_id", area_slug_id)
             result_dict: Dict = self.api.line.get(area=area_slug_id)
-            for n, result in enumerate(result_dict['results']):
-                line_name = result.get('name', '')
-                line_slug_id = result.get('slug')
-                area_slug_id = result.get('area')
-                site_slug_id = result.get('site')
-                line_type = result.get('line_type')
+            for n, result in enumerate(result_dict["results"]):
+                line_name = result.get("name", "")
+                line_slug_id = result.get("slug")
+                area_slug_id = result.get("area")
+                site_slug_id = result.get("site")
+                line_type = result.get("line_type")
 
                 area_info: Dict = self._area[area_slug_id]
-                basic_line_info = {'line_name': line_name, 'line_slug_id': line_slug_id, 
-                                   'area_slug_id': area_slug_id, 'site_slug_id': site_slug_id,
-                                   'line_type': line_type}
+                basic_line_info = {
+                    "line_name": line_name,
+                    "line_slug_id": line_slug_id,
+                    "area_slug_id": area_slug_id,
+                    "site_slug_id": site_slug_id,
+                    "line_type": line_type,
+                }
                 basic_line_info.update(area_info)
                 self._line[line_slug_id] = basic_line_info
-                org = area_info['org']
+                org = area_info["org"]
                 if line_slug_id not in self._tree[org][site_slug_id][area_slug_id]:
                     self._tree[org][site_slug_id][area_slug_id][line_slug_id] = {}
                 print("*" * 40)
                 print("")
                 print(f"n: {n}, ", result)
-
 
     def query_all_devices(self):
         num_lines = len(self._line.keys())
@@ -157,24 +158,27 @@ class QueryFactoryMap:
             print("#" * 140)
             print(f"nn: {nn} out of {num_lines}, line_slug_id", line_slug_id)
             result_dict: Dict = self.api.device.get(parent=line_slug_id)
-            for n, result in enumerate(result_dict['results']):
-                name_label = result.get('label')
-                device_slug_id = result.get('slug')
-                line_slug_id = result.get('parent')
-                connector_type = result.get('connector_type')
-                state = result.get('state')
-
+            for n, result in enumerate(result_dict["results"]):
+                name_label = result.get("label")
+                device_slug_id = result.get("slug")
+                line_slug_id = result.get("parent")
+                connector_type = result.get("connector_type")
+                state = result.get("state")
 
                 line_info: Dict = self._line[line_slug_id]
-                basic_device_info = {'name_label': name_label, 'device_slug_id': device_slug_id,
-                                   'line_slug_id': line_slug_id, 'connector_type': connector_type,
-                                   'state': state}
+                basic_device_info = {
+                    "name_label": name_label,
+                    "device_slug_id": device_slug_id,
+                    "line_slug_id": line_slug_id,
+                    "connector_type": connector_type,
+                    "state": state,
+                }
                 basic_device_info.update(line_info)
                 self._device[device_slug_id] = basic_device_info
-                area_slug_id = basic_device_info.get('area_slug_id')
-                site_slug_id = basic_device_info.get('site_slug_id')
+                area_slug_id = basic_device_info.get("area_slug_id")
+                site_slug_id = basic_device_info.get("site_slug_id")
 
-                org = line_info['org']
+                org = line_info["org"]
                 # import pdb; pdb.set_trace()
                 if device_slug_id not in self._tree[org][site_slug_id][area_slug_id][line_slug_id]:
                     self._tree[org][site_slug_id][area_slug_id][line_slug_id][device_slug_id] = basic_device_info
@@ -182,11 +186,9 @@ class QueryFactoryMap:
                 print("")
                 print(f"n: {n}, ", result)
 
-
-
     def save_result_so_far(self):
         """
-            result4.tmp    ->   self._tree, self._sites, self._area, self._line, self._device
+        result4.tmp    ->   self._tree, self._sites, self._area, self._line, self._device
 
         """
         with open("result5B.tmp", "wb") as writer:
@@ -203,7 +205,6 @@ class QueryFactoryMap:
             print("-" * 60)
             print("")
             print(n, self._device[device_slug_id])
-
 
     def create_factory_map(self):
         org_name_list = self.query_all_orgs()
@@ -226,7 +227,7 @@ class QueryFactoryMap:
 
 
 if __name__ == "__main__":
-    email = 'mbrady@archsys.io'
+    email = "mbrady@archsys.io"
     fm = QueryFactoryMap(email)
     fm.create_factory_map()
     # fm.display_all_devices()
--- setup_login_tokens.py
+++ setup_login_tokens.py
@@ -2,7 +2,8 @@ import os
 
 from cryptography.fernet import Fernet
 
-token_filename = '.cloud_api_token'
+token_filename = ".cloud_api_token"
+
 
 def generate_token_file():
 
@@ -11,25 +12,25 @@ def generate_token_file():
     filename = os.path.abspath(token_filename)
     print(f"resolved filename: {filename}")
 
-    with open(filename, 'wb') as writer:
+    with open(filename, "wb") as writer:
         writer.write(key)
+
 
 def read_token_file():
     filename = os.path.abspath(token_filename)
     print(f"filename: {filename}")
 
-    with open(filename, 'rb') as reader:
+    with open(filename, "rb") as reader:
         key = reader.read()
         return key
 
 
 key = read_token_file()
 f = Fernet(key)
-secret = f.encrypt(b'Gamma!23')
+secret = f.encrypt(b"Gamma!23")
 print(f"secret: {secret}")
 # token = f.encrypt(b"my deep dark secret")
 # token
 # b'...'
 # f.decrypt(token)
 # b'my deep dark secret'
-
